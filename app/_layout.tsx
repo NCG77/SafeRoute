9a83b0fc877cf7b8186f944b65a0a7b69fa1d9ca
NCG77 <swastikmishra7799@gmail.com>
2025-07-27 11:35:58 +0530

    .

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -29,8 +29,8 @@ export default function RootLayout() {
             <SafeAreaProvider>
                 <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
                         <Stack>
-                            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                             <Stack.Screen name="index" options={{ headerShown: false }} />
+                            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
                             <Stack.Screen name="Login" options={{ headerShown: false }} />
                             <Stack.Screen name="Signup" options={{ headerShown: false }} />
                         </Stack>
